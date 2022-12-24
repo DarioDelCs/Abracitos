934b1f880c325fcda14cb09ca5f29cff98b53eb9
DarioDelCs <dariodelc@hotmail.com>
2022-12-24 11:53:12 +0100

    ?

--- Othello_Distribuible/Othello/src/edu/upc/epsevg/prop/othello/Game.java
+++ Othello_Distribuible/Othello/src/edu/upc/epsevg/prop/othello/Game.java
@@ -1,18 +1,9 @@
 package edu.upc.epsevg.prop.othello;
 
-import edu.upc.epsevg.prop.othello.players.HumanPlayer;
-import edu.upc.epsevg.prop.othello.players.RandomPlayer;
-import edu.upc.epsevg.prop.othello.Level;
-import edu.upc.epsevg.prop.othello.IPlayer;
 import edu.upc.epsevg.prop.othello.players.DesdemonaPlayer;
-import edu.upc.epsevg.prop.othello.players.abracitos.PlayerMinimax;
-import edu.upc.epsevg.prop.othello.players.abracitos.Abracitos_IDS_Hash_pruebas;
+import edu.upc.epsevg.prop.othello.players.RandomPlayer;
 import edu.upc.epsevg.prop.othello.players.abracitos.PlayerID;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import edu.upc.epsevg.prop.othello.players.abracitos.PlayerMinimax;
 
 
 import javax.swing.SwingUtilities;
@@ -32,16 +23,17 @@ public class Game {
             @Override
             public void run() {
                 //IPlayer player2 = new Abracitos_IDS_Hash("A_Hash", 1);
-                IPlayer player1 = new PlayerID(2);
+//                IPlayer player1 = new PlayerID(2);
 //                  IPlayer player1 = new Abracitos_IDS_anillo();
 //                IPlayer player1 = new Abracitos_IDS_Hash_pruebas("winner", 2);
 //                IPlayer player1 = new HumanPlayer("Human1");
-//                IPlayer player2 = new PlayerMinimax(7);
+                IPlayer player1 = new PlayerMinimax(8);
                 IPlayer player2 = new DesdemonaPlayer(1);//GB
+//                IPlayer player2 = new RandomPlayer("Random");
 
                 //player 1 no_ids vs player 2 ids peta
                                 
-                new Board(player1 , player2, 4, false);
+                new Board(player1 , player2, 2, false);
             }
         });
     }
